b8882452ff88886b00b2bf7dd7715a60466f073d
clequinio <kikil592@gmail.com>
2020-11-29 02:02:03 +0000

    commit

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=clequinio/ml-microservice-k8s-aws
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username clequinio
+docker tag c1f0f62a3e4e $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
