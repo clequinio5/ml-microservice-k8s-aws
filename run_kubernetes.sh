a478f2de3887b1ca3f45e6aff6905f85a0ed9276
clequinio <kikil592@gmail.com>
2020-11-29 02:31:53 +0000

    commit

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=clequinio/ml-microservice-k8s-aws
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+minikube kubectl -- run --image=$dockerpath app
 
 
 # Step 3:
 # List kubernetes pods
+minikube kubectl -- get pod
 
 # Step 4:
 # Forward the container port to a host
+minikube kubectl -- port-forward pod/app 8000:80
 
